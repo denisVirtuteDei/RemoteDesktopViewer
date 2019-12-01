b6b442aacdbe5af923a5130f1fa42243435ce35f
Denis <41122226+denisVirtuteDei@users.noreply.github.com>
2019-12-01 21:37:57 +0300

    WithoutKey&DX

--- NotTeamViewer/MainWindow.xaml.cs
+++ NotTeamViewer/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Windows;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
-using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using System.Windows;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace NotTeamViewer.Server
 {
@@ -12,8 +15,12 @@ namespace NotTeamViewer.Server
     public partial class MainWindow : Window
     {
         private readonly TcpServer tcp;
-        private readonly int width = 1920;
-        private readonly int height = 1080;
+        private int width = 1920;
+        private int height = 1080;
+        private MouseFlags prevL = MouseFlags.Absolute,
+                           prevR = MouseFlags.Absolute;
+        private char keyPrev = default;
+        
 
 
         public MainWindow()
@@ -21,16 +28,6 @@ namespace NotTeamViewer.Server
             InitializeComponent();
             tcp = new TcpServer(this);
             tcp.MouseMoveNotify += Tcp_MouseMoveNotify;
-        }
-
-        private void Tcp_MouseMoveNotify(string str)
-        {
-            var loc = str.Split(' ');
-            int left = loc[2] == "1" ? 1 : 0;
-            double w = Convert.ToDouble(loc[0]) * width;
-            double h = Convert.ToDouble(loc[1]) * height;
-            SetCursorPos((int)w, (int)h);
-            //var ev = new MouseEventArgs(MouseButtons.Left, left, 630, 630, 0);
         }
 
         private async void StartServer()
@@ -41,7 +38,7 @@ namespace NotTeamViewer.Server
                 await Task.Run(() => tcp.Run());
             }
         }
-
+        
         private void ChangeColor_Click(object sender, RoutedEventArgs e)
         {
 
@@ -54,7 +51,7 @@ namespace NotTeamViewer.Server
 
         private void CheckBox_Click(object sender, RoutedEventArgs e)
         {
-            System.Windows.MessageBox.Show("Value changed");
+            MessageBox.Show("Value changed");
         }
 
         private void StartItem_Click(object sender, RoutedEventArgs e)
@@ -71,9 +68,90 @@ namespace NotTeamViewer.Server
                 StartServer();
             }
         }
+        
+
+        private void Tcp_MouseMoveNotify(byte[][] commands)
+        {
+            InputSimulator input = new InputSimulator();
+
+            double w = BitConverter.ToDouble(commands[0], 0) * (width*34);
+            double h = BitConverter.ToDouble(commands[1], 0) * (height*60);
+            char leftStatus = BitConverter.ToChar(commands[2], 0);
+            char rightStatus = BitConverter.ToChar(commands[3], 0);
+            char keyStatus = BitConverter.ToChar(commands[4], 0);
+            VirtualKeyCode key = (VirtualKeyCode)BitConverter.ToInt32(commands[5], 0);
+            int delta = BitConverter.ToInt32(commands[6], 0);
+            
+
+            mouse_event(MouseFlags.Absolute | MouseFlags.Move, (int)w, (int)h, 0, UIntPtr.Zero);
+            mouse_event(MouseFlags.MouseWheel, (int)w, (int)h, unchecked(delta), UIntPtr.Zero);
+
+            if (leftStatus == 'd')
+            {
+                mouse_event(MouseFlags.LeftDown | MouseFlags.Absolute, (int)w, (int)h, 0, UIntPtr.Zero);
+                prevL = MouseFlags.LeftDown;
+            }
+            else if (leftStatus == 'u' && prevL == MouseFlags.LeftDown)
+            {
+                mouse_event(MouseFlags.LeftUp | MouseFlags.Absolute, (int)w, (int)h, 0, UIntPtr.Zero);
+                prevL = MouseFlags.LeftUp;
+            }
+            else
+            {
+                mouse_event(MouseFlags.Absolute, (int)w, (int)h, 0, UIntPtr.Zero);
+                prevL = MouseFlags.Absolute;
+            }
+
+            if (rightStatus == 'd')
+            {
+                mouse_event(MouseFlags.RightDown | MouseFlags.Absolute, (int)w, (int)h, 0, UIntPtr.Zero);
+                prevR = MouseFlags.RightDown;
+            }
+            else if (rightStatus == 'u' && prevR == MouseFlags.RightDown)
+            {
+                mouse_event(MouseFlags.RightUp | MouseFlags.Absolute, (int)w, (int)h, 0, UIntPtr.Zero);
+                prevR = MouseFlags.RightUp;
+            }
+            else
+            {
+                mouse_event(MouseFlags.Absolute, (int)w, (int)h, 0, UIntPtr.Zero);
+                prevR = MouseFlags.Absolute;
+            }
 
 
-        [DllImport("user32.dll")]
-        public static extern void SetCursorPos(int x, int y);
+            if (keyStatus == 'd')
+            {
+                input.Keyboard.KeyDown(key);
+                keyPrev = keyStatus;
+            }
+            else if (keyStatus == 'u' && keyPrev == 'd')
+            {
+                input.Keyboard.KeyUp(key);
+                keyPrev = 'u';
+            }
+            else
+                keyPrev = 'f';
+
+
+        }
+
+
+
+        [Flags]
+        enum MouseFlags
+        {
+            LeftDown = 0x00000002,
+            LeftUp = 0x00000004,
+            MiddleDown = 0x00000020,
+            MiddleUp = 0x00000040,
+            Move = 0x00000001,
+            Absolute = 0x00008000,
+            RightDown = 0x00000008,
+            RightUp = 0x00000010,
+            MouseWheel = 0x0800
+        };
+
+        [DllImport("User32.dll", SetLastError = true)]
+        static extern void mouse_event(MouseFlags dwFlags, int dx, int dy, int dwData, UIntPtr dwExtraInfo);
     }
 }
